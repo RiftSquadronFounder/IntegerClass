85f52e96ce007ba679c59321a4eba3a6e7c845f5
Yarik Vodila <109005417+RiftSquadronFounder@users.noreply.github.com>
2025-05-18 15:51:01 +0500

    Update Main.cpp

--- IntegerClass/Main.cpp
+++ IntegerClass/Main.cpp
@@ -8,16 +8,15 @@
 
 int main() {
 
-
 	Fraction frac1 = Fraction(5, 20);
-	Fraction frac2 = Fraction(5, 20);
+	Fraction frac2 = Fraction(75, 20);
 
-	Fraction ans = (frac1 + frac2);
+	Fraction ans = (frac1 - frac2);
 	std::cout << frac1 << std::endl;
 
 	std::cout << frac2 << std::endl;
 
-	std::cout << ans << std::endl;
+	std::cout <<frac1 <<" - " <<frac2 << " = " << ans << std::endl;
 
 
 	
