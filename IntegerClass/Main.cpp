4c048472d751fbbf7a615dcf660e5291b84605b5
Echo_1-1 <fya17102007@gmail.com>
2025-06-03 12:51:26 +0500

    Прописан тест сериализации в файле main.cpp

--- IntegerClass/Main.cpp
+++ IntegerClass/Main.cpp
@@ -7,7 +7,7 @@
 
 
 int main() {
-
+	/*
 	Fraction frac1 = Fraction(5, 20);
 	Fraction frac2 = Fraction(75, 20);
 
@@ -17,8 +17,27 @@ int main() {
 	std::cout << frac2 << std::endl;
 
 	std::cout <<frac1 <<" - " <<frac2 << " = " << ans << std::endl;
+	*/
+
+	Integer value = 12;
+	std::cout <<"Initial value: " << value << "\n\n";
+
+	
+	value.Serialize("FileName.txt");
+	std::cout << "Value Serialized successfully\n\n\n\n";
+	
+	std::string fileName = "FileName";
+
+	Integer valueSerTest = 0;
+
+	std::cout << "Value two before deserealize: " << valueSerTest << "\n\n";
+
+	
+	valueSerTest.Deserialize(fileName + ".txt");
+	std::cout << "Value Deserealized from file: \"" << fileName << "\" successfully\n\n";
 
 
+	std::cout << "Value two after deserealize: " << valueSerTest << "\n\n\n";
 	
 
 	return 0;
