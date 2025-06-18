1688b5c6cc270dac19612d095be9178f66536d2f
Echo 1-1 <fya17102007@gmail.com>
2025-06-18 13:08:59 +0500

    Произведена компиляция в библиотеку

---
+++ IntegerClassLib/IntegerClassLib.cpp
@@ -0,0 +1,21 @@
+﻿// IntegerClassLib.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+#include "../IntegerClass/Fraction.hpp"
+#include "../IntegerClass/IntegerH.hpp"
+#include "../IntegerClass/Matrix.hpp"
+#include "../IntegerClass/Point2D.hpp"
+#include "../IntegerClass/Real.hpp"
+#include "../IntegerClass/ScalarStrategy.hpp"
+#include "../IntegerClass/Shape2D.hpp"
+#include "../IntegerClass/Vector2D.hpp"
+#include "../IntegerClass/VectorProcessingStrategy.hpp"
+#include "../IntegerClass/VectorSumStrategy.hpp"
+
+// TODO: Это пример библиотечной функции.
+void fnIntegerClassLib()
+{
+}
